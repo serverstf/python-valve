be88f34c28e962f26f8208161a06b5e3ea406ae7
unknown <ottajay@googlemail.com>
2013-07-04 18:58:31 +0100

    Implemented 'correct' RCON authentification
    Support for multi-packet responses technically implemented,
    however executing commands like 'cvarlist' causes hang.

--- steam/rcon.py
+++ steam/rcon.py
@@ -1,10 +1,15 @@
 
+"""
+	Provides an interface to the Source Dedicated Server (SRCDS) remote
+	console (RCON), allow you to issue commands to a server remotely.
+"""
+
 import socket
 import struct
 import select
 
 class IncompleteMessageError(Exception): pass
-class NotAuthenticatedError(Exception): pass
+class AuthenticationError(Exception): pass
 
 class Message(object):
 	
@@ -29,7 +34,8 @@ class Message(object):
 						Message.SERVERDATA_RESPONSE_VALUE: "SERVERDATA_RESPONSE_VALUE"
 					}.get(self.type, "INVALID"),
 					id=self.id,
-					body=self.body)
+					body=" ".join([c.encode("hex") for c in self.body])
+					)
 	
 	@property
 	def size(self):
@@ -79,6 +85,7 @@ class RCON(object):
 		self._next_id = 1
 		self._read_buffer = ""
 		self._active_requests = {}
+		self._response = []
 		
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.connect((self.host, self.port))
@@ -91,10 +98,17 @@ class RCON(object):
 	def request(self, type, body=u""):
 		
 		request = Message(self._next_id, type, body)
+		#print ">>", request
 		self._active_requests[request.id] = request
 		self._next_id += 1
 		
 		self.socket.send(request.encode())
+		# Must send a SERVERDATA_RESPONSE_VALUE after EXECCOMMAND
+		# in order to handle multi-packet responses as per
+		# https://developer.valvesoftware.com/wiki/RCON#Multiple-packet_Responses
+		if type == Message.SERVERDATA_EXECCOMAND:
+			self.request(Message.SERVERDATA_RESPONSE_VALUE)
+		
 		return request
 	
 	def process(self):
@@ -104,7 +118,29 @@ class RCON(object):
 			self._read_buffer += self.socket.recv(4096)
 		
 		response, self._read_buffer = Message.decode(self._read_buffer)
-		self._active_requests[response.id].response = response
+		
+		#print "<<", response
+		# Check if terminating RESPONSE_VALUE with body
+		# 0x0000 0001 0000 0000
+		if response.type == Message.SERVERDATA_RESPONSE_VALUE and \
+			response.body.encode("ascii") == "\x00\x01\x00\x00":
+				
+			response = Message(self._response[0].id,
+										self._response[0].type,
+										"".join([r.body for r in self._response])
+									)
+			self._active_requests[response.id].response = response
+			self._response = []
+			
+		elif response.type == Message.SERVERDATA_RESPONSE_VALUE:
+			self._response.append(response)
+			
+		elif response.type == Message.SERVERDATA_AUTH_RESPONSE:
+			self._active_requests[self._response[0].id].response = response
+			
+			# Clear empty SERVERDATA_RESPONSE_VALUE sent before
+			# SERVERDATA_AUTH_RESPONSE
+			self._response = []
 	
 	def response_to(self, request, timeout=10.0):
 		"""
@@ -139,12 +175,21 @@ class RCON(object):
 		
 		request = self.request(Message.SERVERDATA_AUTH, unicode(password))
 		with self.response_to(request) as response:
-			print response
+			if response.id == -1:
+				raise AuthenticationError("Bad password")
+				
 			self.is_authenticated = True
 		
-	def execute(self, command):
+	def execute(self, command, block=True):
+		
+		if not self.is_authenticated:
+			raise AuthenticationError
 		
 		request = self.request(Message.SERVERDATA_EXECCOMAND, unicode(command))
+		if block:
+			with self.response_to(request):
+				pass
+				
 		return request
 		
 
