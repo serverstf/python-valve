7fa03cce96d831a61e0317213a411dfb5064ef95
Oliver Ainsworth <ottajay@googlemail.com>
2015-10-18 01:43:47 +0100

    Make A2S functional tests work with Python 3

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2014 Oliver Ainsworth
+# Copyright (C) 2014-2015 Oliver Ainsworth
 
 from __future__ import (absolute_import,
                         unicode_literals, print_function, division)
 
-
 import pytest
-
+import six
 
 import valve.source.a2s
 
@@ -30,7 +29,7 @@ def test_tf2_info(address):
         pytest.skip("Timedout waiting for response")
     assert info["app_id"] == 440
     assert info["folder"] == "tf"
-    assert isinstance(info["folder"], unicode)
+    assert isinstance(info["folder"], six.text_type)
 
 
 @pytest.srcds_functional(gamedir="cstrike")
@@ -52,7 +51,7 @@ def test_css_info(address):
         return
     assert info["app_id"] == 240
     assert info["folder"] == "cstrike"
-    assert isinstance(info["folder"], unicode)
+    assert isinstance(info["folder"], six.text_type)
 
 
 @pytest.srcds_functional(gamedir="csgo")
@@ -74,7 +73,7 @@ def test_csgo_info(address):
         return
     assert info["app_id"] == 730
     assert info["folder"] == "csgo"
-    assert isinstance(info["folder"], unicode)
+    assert isinstance(info["folder"], six.text_type)
 
 
 @pytest.srcds_functional(gamedir="dota")
@@ -96,7 +95,7 @@ def test_dota2_info(address):
         return
     assert info["app_id"] == 570
     assert info["folder"] == "dota"
-    assert isinstance(info["folder"], unicode)
+    assert isinstance(info["folder"], six.text_type)
 
 
 @pytest.srcds_functional(gamedir="left4dead")
@@ -118,7 +117,7 @@ def test_l4d_info(address):
         return
     assert info["app_id"] == 500
     assert info["folder"] == "left4dead"
-    assert isinstance(info["folder"], unicode)
+    assert isinstance(info["folder"], six.text_type)
 
 
 @pytest.srcds_functional(gamedir="left4dead2")
@@ -140,4 +139,4 @@ def test_l4d2_info(address):
         return
     assert info["app_id"] == 550
     assert info["folder"] == "left4dead2"
-    assert isinstance(info["folder"], unicode)
+    assert isinstance(info["folder"], six.text_type)
