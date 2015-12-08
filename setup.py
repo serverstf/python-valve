8cc2e5f98fb15fa72b2b6ef27351208f4b03defa
Oliver Ainsworth <ottajay@googlemail.com>
2015-12-08 20:33:22 +0000

    Temporarily disable some test requirements

--- setup.py
+++ setup.py
@@ -47,9 +47,10 @@ setup(
     install_requires=install_requires,
     tests_require=[
         "mock==1.0.1",
-        "pytest>=2.8.0",
-        "pytest-capturelog",
-        "pytest-timeout",
+        "pytest",
+        # "pytest>=2.8.0",
+        # "pytest-capturelog",
+        # "pytest-timeout",
     ],
     extras_require={
         "development": [
